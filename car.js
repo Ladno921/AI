2a72b605c900820ce78525d9f0a77e8970c9f142
Ladno921 <ivan_shelyag_49@mail.com>
2022-09-23 09:31:00 +0300

    update

---
+++ car.js
@@ -0,0 +1,103 @@
+class Car {
+  constructor(x, y, width, height, color = 'blue') { //100, 100, 30, 50
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
+
+    this.sensor = new Sensor(this);
+    this.controls = new Controls();
+    //img
+    this.img = new Image();
+    this.img.src = "car.png";
+
+    this.mask = document.createElement("canvas");
+    this.mask.width = width;
+    this.mask.height = height;
+
+    const maskCtx = this.mask.getContext("2d");
+    this.img.onload = () => {
+      maskCtx.fillStyle = color;
+      maskCtx.rect(0, 0, this.width, this.height);
+      maskCtx.fill();
+
+      maskCtx.globalCompositeOperation = "destination-atop";
+      maskCtx.drawImage(this.img, 0, 0, this.width, this.height);
+    };
+  }
+
+
+  move() {
+    this.sensor.update();
+
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.rect(
+      - this.width / 2,
+      - this.height / 2,
+      this.width,
+      this.height,
+    );
+
+    ctx.fill();
+
+    ctx.drawImage(
+      this.img,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+
+    ctx.restore();
+    this.sensor.draw(ctx);
+  }
+}
